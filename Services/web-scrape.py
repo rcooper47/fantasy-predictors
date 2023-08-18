98e805275da191456a43f42cca7fc70efef8e725
ryan cooper <3RCOOPER8@GMAIL.COM>
2023-08-18 11:02:48 -0400

    Stats Excel Init, Add Fumble-Combiner

--- Services/web-scrape.py
+++ Services/web-scrape.py
@@ -1,20 +1,19 @@
 import requests 
 from bs4 import BeautifulSoup
 import nfl_data_py as nfl
-
+import pandas 
 URL = "https://api.sportsdata.io/api/nfl/fantasy/json/Standings/2022REG"
 page = requests.get(URL)
 
-#soup = BeautifulSoup(page.content, "html.parser")
-#class_name = "k-grid k-widget k-display-block k-grid-lockedcolumns"
-
-#results = soup.find("div", class_="k-grid k-widget k-display-block k-grid-lockedcolumns")
-
-#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-#driver.get(URL)
-#players = driver.find_elements_by_xpath('//td[@class="k-grid k-widget k-display-block k-grid-lockedcolumns"]')
-#content = driver.find_element(By.CLASS_NAME, class_name)
 stats = nfl.import_weekly_data([2022,2021])
+# [id, name, week, f-score, stat1, stat2, avg-pts-agst,..., ]
 
-print(stats)
+head = stats.head(100)
 
+head.to_excel('output.xlsx', index=False)
+
+### Combine fumbles lost columns
+def fumble_combiner(df):
+    df["fumbles_lost"] = df["sack_fumbles_lost"] + df["rushing_fumbles_lost"] +df["receiving_fumbles_lost"]
+    return df
+### Write tests
