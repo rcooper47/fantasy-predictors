0879961662cff9b196408b80a39fe18136b653e3
ryan cooper <3RCOOPER8@GMAIL.COM>
2023-08-30 15:48:32 -0400

    Add Player Search
    Co-Authored-By: zzimmerman7 <116845703+zzimmerman7@users.noreply.github.com>

--- Services/web-scrape.py
+++ Services/web-scrape.py
@@ -10,13 +10,36 @@ stats = nfl.import_weekly_data([2022,2021])
 
 head = stats.head(100)
 
-head.to_excel('output.xlsx', index=False)
+#head.to_excel('output.xlsx', index=False)
 
 ### Combine fumbles lost columns
 def fumble_combiner(df):
     df["fumbles_lost"] = df["sack_fumbles_lost"] + df["rushing_fumbles_lost"] + df["receiving_fumbles_lost"]
     return df
+
+"""
+^^^ SettingWithCopyWarning: 
+A value is trying to be set on a copy of a slice from a DataFrame.
+Try using .loc[row_indexer,col_indexer] = value instead
+"""
+
 def reg_season_combiner(df):
     regseason = stats[stats['season_type']== "REG"]
     return regseason
+
+def remove_kickers(df):
+    no_kickers = stats[stats['position'] != "SPEC"]
+    return no_kickers
+
+def player_searcher(df, name):
+    player_stats = stats[stats['player_display_name'] == name]
+    return player_stats
+
+def main(df):
+    df = fumble_combiner(df)
+    df = reg_season_combiner(df)
+    df = remove_kickers(df)
+    return df
+
+print(player_searcher(stats, "Tom Brady"))
 ### Write tests
